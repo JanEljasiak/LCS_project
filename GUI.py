55f00929dbefeff74b321129f385fdd96e866018
Karol <karol.suchecki99@gmail.com>
2022-11-18 11:42:17 +0100

    Add input window

--- GUI.py
+++ GUI.py
@@ -2,11 +2,23 @@ import PySimpleGUI as sg  # pip install pysimplegui
 
 
 layout = [
-    [sg.Text("Input data: "), sg.Input(key="-IN-"), sg.FileBrowse()],
-    [sg.Text("Output data: "), sg.Input(key="-OUT-"), sg.FileBrowse()],
-    [sg.Exit(), sg.Button("Do sth")],
+    [sg.Text("Input data: "), sg.Input(key="-IN-"), sg.FileBrowse(file_types=(("TextFiles", "*.txt"),("Fasta files","*.fasta")))],
+    [sg.Text("Output folder: "), sg.Input(key="-OUT-"), sg.FolderBrowse()],
+    [sg.Exit(), sg.Button("Check degree of kinship")],
 ]
 
-window = sg.Window("123", layout)
+window = sg.Window("Kinshaap", layout)
 
 while True:
+    event, values = window.read()
+
+    if event in (sg.WINDOW_CLOSED, "Exit"):
+        break
+    if event == "Check degree of kinship":
+        sg.popup_error("Not yet implemented")
+window.close()
+"""
+
+
+
+"""
