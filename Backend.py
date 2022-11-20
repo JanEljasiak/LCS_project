203c736f4ae28c3f0c83ef9cbb66d86aa585d4d5
damian830 <damian_33302@wp.pl>
2022-11-20 21:59:23 +0100

    Do small changes in two first functions

--- Backend.py
+++ Backend.py
@@ -1,10 +1,8 @@
 # W tym pliku znajduje się nasza implementacja algorytmu FLCS wraz z potrzebymi funkcjami
 
 def build_successor_tables(seqA, seqB):
-    seqAWithSpace = " " + seqA
-    n1 = len(seqAWithSpace)
-    seqBWithSpace = " " + seqB
-    n2 = len(seqBWithSpace)
+    seqAWithSpace, seqBWithSpace = " " + seqA, " " + seqB
+    n1, n2 = len(seqAWithSpace), len(seqBWithSpace)
     distinctLetters = "".join(dict.fromkeys(seqA + seqB))
     m = len(distinctLetters)
     TseqA = [[-1 for col in range(n1)] for row in range(m)]
@@ -14,11 +12,13 @@ def build_successor_tables(seqA, seqB):
             TseqA[i][j] = seqAWithSpace.find(distinctLetters[i], j+1)
         for j in range(n2):
             TseqB[i][j] = seqBWithSpace.find(distinctLetters[i], j+1)
-    return [TseqA, TseqB]
+    return TseqA, TseqB
 
-def pairs(TseqA, TseqB):
+def pairs(T):
+    TseqA, TseqB = T
     n = len(TseqA)
     pairs_table = [None]*n
     for i in range(n):
-        pairs_table[i] = [i, TseqA[i][0], TseqB[i][0], 0, 1]
+        pairs_table[i] = [i, TseqA[i][0], TseqB[i][0], 0, -1, 1]
+                            # (k,i,j,level,pre,active)
     return pairs_table
