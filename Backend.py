59613176c5d46df988d599e83a35530a068cdc02
damian830 <damian_33302@wp.pl>
2022-11-21 10:36:43 +0100

    Change names of variables

--- Backend.py
+++ Backend.py
@@ -12,27 +12,25 @@ def build_successor_tables(seqA, seqB):
             TseqA[i][j] = seqAWithSpace.find(distinctLetters[i], j+1)
         for j in range(n2):
             TseqB[i][j] = seqBWithSpace.find(distinctLetters[i], j+1)
-    return TseqA, TseqB
+    return TseqA, TseqB, m
 
-def pairs(T):
-    TseqA, TseqB = T
-    n = len(TseqA)
-    pairsTable = [None]*n
-    for i in range(n):
+def pairs(matricesWithRowDim):
+    TseqA, TseqB, m = matricesWithRowDim
+    pairsTable = [None]*m
+    for i in range(m):
         pairsTable[i] = [i, TseqA[i][0], TseqB[i][0], 0, -1, 1]
                             # (k,i,j,level,pre,active)
     return pairsTable
 
-def pairs_complete(T, pairsTable):
-    TseqA, TseqB = T
-    n = len(TseqA)
+def pairs_complete(matricesWithRowDim, pairsTable):
+    TseqA, TseqB, m = matricesWithRowDim
     while any(item[5] == 1 for item in pairsTable):
         for identicalPair in filter(lambda x: x[5] == 1, pairsTable):
-            k = max(x[0] for x in pairsTable) + 1
-            for i in range(n):
+            maximalRow = max(x[0] for x in pairsTable) + 1
+            for i in range(m):
                 el1 = TseqA[i][identicalPair[1]]
                 el2 = TseqB[i][identicalPair[2]]
                 if el1 != -1 and el2 != -1:
-                    pairsTable.append([k + i, el1, el2, identicalPair[3] + 1, identicalPair[0], 1])
+                    pairsTable.append([maximalRow + i, el1, el2, identicalPair[3] + 1, identicalPair[0], 1])
             pairsTable[pairsTable.index(identicalPair)][5] = 0
     return pairsTable
