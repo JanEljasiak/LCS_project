38f3c66a836fb6b840023aec279f95940564ce95
karol <karol.suchecki99@gmail.com>
2022-11-28 10:56:46 +0100

    Add GUI window

--- Backend.py
+++ Backend.py
@@ -1,25 +1,56 @@
-# W tym pliku znajduje się nasza implementacja algorytmu FLCS wraz z potrzebymi funkcjami
+# W tym pliku znajduje się nasza implementacja algorytmu FLCS wraz z potrzebnymi funkcjami
 
-def funkcja():
-    Chcesz_sie_bawic = True
-    while Chcesz_sie_bawic:
+def build_successor_tables(seqA, seqB):
+    seqAWithSpace, seqBWithSpace = " " + seqA, " " + seqB
+    colDimA, colDimB = len(seqAWithSpace), len(seqBWithSpace)
+    distinctLetters = "".join(dict.fromkeys(seqA + seqB))
+    rowDim = len(distinctLetters)
+    TseqA = [[-1 for col in range(colDimA)] for row in range(rowDim)]
+    TseqB = [[-1 for col in range(colDimB)] for row in range(rowDim)]
+    for i in range(rowDim):
+        for j in range(colDimA):
+            TseqA[i][j] = seqAWithSpace.find(distinctLetters[i], j+1)
+        for j in range(colDimB):
+            TseqB[i][j] = seqBWithSpace.find(distinctLetters[i], j+1)
+    return TseqA, TseqB, rowDim
 
-        licznik = int(input("Podaj liczbę od 1 do 5 (poza 4)"))
-        if licznik not in [1, 2, 3, 5]:
-            print("Bardzo nieładne zachowanie ;/ ")
-            Chcesz_sie_bawic = False
-        if licznik == 1:
-            print("Janek pozdrawia prowadzacego!")
-        if licznik == 2:
-            print("Damian pozdrawia prowadzacego!")
-        if licznik == 3:
-            print("Karol pozdrawia prowadzacego!")
-        if licznik == 5:
-            print("Wszyscy pozdrawiamy prowadzacego!")
-        Chcesz_sie_bawic = bool(input("Czy dalej chcesz sie bawić? (Tak- wpisz cokolwiek/dosyć tego- zostaw puste pole)"))
-        print("Miłego dnia")
+def pairs(matricesWithRowDim):
+    TseqA, TseqB, rowDim = matricesWithRowDim
+    pairsTable = [None]*rowDim
+    for i in range(rowDim):
+        pairsTable[i] = [i, TseqA[i][0], TseqB[i][0], 0, -1, 1]
+                            # (k,i,j,level,pre,active)
+    return pairsTable
 
+def pairs_complete(matricesWithRowDim, pairsTable):
+    TseqA, TseqB, rowDim = matricesWithRowDim
+    while any(item[5] == 1 for item in pairsTable):
+        for identicalPair in filter(lambda x: x[5] == 1, pairsTable):
+            maximalRow = max(x[0] for x in pairsTable) + 1
+            for i in range(rowDim):
+                el1 = TseqA[i][identicalPair[1]]
+                el2 = TseqB[i][identicalPair[2]]
+                if el1 != -1 and el2 != -1:
+                    pairsTable.append([maximalRow + i, el1, el2, identicalPair[3] + 1, identicalPair[0], 1])
+            pairsTable[pairsTable.index(identicalPair)][5] = 0
+    return pairsTable
 
-funkcja()
+def find_list_of_LCS(pairsTable, seqA):
+    seqAWithSpace = " " + seqA
+    maximalLevel = max(x[3] for x in pairsTable)
+    identicalPairsWithMaximalLevel = list(filter(lambda x: x[3] == maximalLevel, pairsTable))
+    numberOfLCS = len(identicalPairsWithMaximalLevel)
+    listOfLCS = [None] * numberOfLCS
+    for i, identicalPair in enumerate(identicalPairsWithMaximalLevel):
+        tempPair = identicalPair
+        LCS = seqAWithSpace[tempPair[1]]
+        for j in range(maximalLevel):
+            tempPair = list(filter(lambda x: x[0] == tempPair[4], pairsTable))[0]
+            LCS = seqAWithSpace[tempPair[1]] + LCS
+        listOfLCS[i] = LCS
+    return listOfLCS
 
-
+def LCS(seqA, seqB):
+    matricesWithRowDim = build_successor_tables(seqA, seqB)
+    pairsTable = pairs_complete(matricesWithRowDim, pairs(matricesWithRowDim))
+    return find_list_of_LCS(pairsTable, seqA)
--- Unit tests.py
+++ Unit tests.py
@@ -1,11 +1,28 @@
 #W tym pliku znajdują się testy jednostkowe
 import unittest
-
+import Backend
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_build_successor_tables(self):
+        # given
+        seqA="TAGCCAT"
+        seqB="AGTGCAC"
+        expected=([[1, 7, 7, 7, 7, 7, 7, -1],[2, 2, 6, 6, 6, 6, -1, -1],[3, 3, 3, -1, -1, -1, -1, -1],[4, 4, 4, 4, 5, -1, -1, -1]],[[3, 3, 3, -1, -1, -1, -1, -1],[1, 6, 6, 6, 6, 6, -1, -1], [2, 2, 4, 4, -1, -1, -1, -1],[5, 5, 5, 5, 5, 7, 7, -1]],4)
+        # when
+        result= Backend.build_successor_tables("TAGCCAT","AGTGCAC")
+        #then
+        self.assertEqual(result, expected)
+    def test_build_successor_tables2(self):
+        # given
+        seqA="AGCA"
+        seqB="AGAAG"
+        expected=([[1,4,4,4,-1],[2,2,-1,-1,-1],[3,3,3,-1,-1]],[[1,3,3,4,-1,-1],[2,2,5,5,5,-1],[-1,-1,-1,-1,-1,-1]],3)
+        # when
+        result= Backend.build_successor_tables("AGCA","AGAAG")
+        #then
+        self.assertEqual(result, expected)
+
 
 
 if __name__ == '__main__':
